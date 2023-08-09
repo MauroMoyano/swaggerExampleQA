211b1a3944498a6a40d472f21856c9bc71a23109
MauroMoyano <mauromoyanodev@gmail.com>
2023-08-09 01:58:12 -0300

    SwaggerOk

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+// Swagger
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+import { options } from './swaggerOptions'
+
 import taskRoutes from './routes/tasks.routes';
 
 const app = express();
@@ -12,6 +17,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+const specs = swaggerJSDoc(options)
+
 app.use(taskRoutes)
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 export default app;
